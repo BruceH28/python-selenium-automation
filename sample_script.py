5f84d32ba28cba31a423b24b301b90db87ff9a3c
BruceH28 <vhlumiere1@gmail.com>
2025-12-31 17:30:36 -0500

    Update automated search script

--- sample_script.py
+++ sample_script.py
@@ -13,23 +13,21 @@ driver = webdriver.Chrome(service=service)
 driver.maximize_window()
 
 # open the url
-driver.get('https://www.google.com/?zx=1767214650943&no_sw_cr=1')
+driver.get('https://www.google.com/')
 
 # populate search field
 search = driver.find_element(By.NAME, 'q')
 search.clear()
-search.send_keys('Table')
+search.send_keys('Car')
 
 # wait for 4 sec
 sleep(4)
 
 # click search button
 driver.find_element(By.NAME, 'btnK').click()
-sleep(5)
 
 # verify search results
-assert 'table'.lower() in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
+assert 'car'.lower() in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
 print('Test Passed')
-
 
 driver.quit()
